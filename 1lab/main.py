c285c634a6bf0001b8a13bf4eb194ca395e3f437
veralabuzova <verka19082007@gmail.com>
2025-10-30 22:47:11 +0300

    Update main.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,4 +1,23 @@
+import random
+N = 10
+x = list(range(1, N+1))
 
+def shuffle_cards(x):
+    shuffled = x.copy()
+    for i in range(len(shuffled)-1, 0, -1):
+        j = random.randint(0, i)
+        shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
+    return shuffled
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+target_number = 7
+position_counts = [0] * N
+
+for _ in range(1000):
+    shuffled = shuffle_cards(x)
+    for position, number in enumerate(shuffled):
+        if number == target_number:
+            position_counts[position] += 1
+
+print(f"\nСколько раз число {target_number} было на каждой позиции:")
+for i in range(N):
+    print(f"Позиция {i+1}: {position_counts[i]} раз")
